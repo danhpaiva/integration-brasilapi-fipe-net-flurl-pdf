6ce085cc38a9567785fad236184de14518095ee1
Paiva <danpaiva@live.com>
2026-02-01 19:23:32 -0300

    feat: add projeto de domain

---
+++ Integration.BrasilApi.Fipe.Net/Integration.BrasilApi.Fipe.Domain/Models/VeiculoFipeResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Integration.BrasilApi.Fipe.Domain.Models;
+
+public record VeiculoFipeResponse(
+    string Valor,
+    string Marca,
+    string Modelo,
+    int AnoModelo,
+    string Combustivel,
+    string CodigoFipe,
+    string MesReferencia,
+    int TipoVeiculo,
+    string SiglaCombustivel,
+    string DataConsulta
+);
