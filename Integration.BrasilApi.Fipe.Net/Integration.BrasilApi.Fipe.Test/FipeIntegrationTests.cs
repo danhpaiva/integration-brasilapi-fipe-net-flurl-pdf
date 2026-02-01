9923fd45328312232a6a3cab9989e2a6e872ca73
Paiva <danpaiva@live.com>
2026-02-01 19:37:43 -0300

    feat: add tests

---
+++ Integration.BrasilApi.Fipe.Net/Integration.BrasilApi.Fipe.Test/FipeIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using Integration.BrasilApi.Fipe.Infrastructure.Services;
+
+namespace Integration.BrasilApi.Fipe.Test;
+
+public class FipeIntegrationTests
+{
+    [Fact]
+    public async Task ObterPreco_DeveRetornarVeiculo_QuandoCodigoValido()
+    {
+        // Arrange
+        var service = new FipeService(); // Em prod usaríamos Mock do Http
+        var codigoValido = "001004-9";
+
+        // Act
+        var resultado = await service.ObterPrecoAsync(codigoValido);
+
+        // Assert
+        Assert.NotNull(resultado);
+        Assert.Equal("Fiat", resultado.Marca);
+        Assert.Contains("Palio", resultado.Modelo);
+    }
+
+    [Fact]
+    public async Task ObterPreco_DeveGerarArquivoNoDesktop()
+    {
+        // Arrange
+        var service = new FipeService();
+        var codigo = "001004-9";
+        var esperadoPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), $"Fipe_{codigo}.pdf");
+
+        // Act
+        await service.ObterPrecoAsync(codigo);
+
+        // Assert
+        Assert.True(File.Exists(esperadoPath));
+    }
+}
